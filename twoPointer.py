12fce152cf2c9c5a2842a5dbff52c4944e778379
TharunReddy07 <tharun.reddy0706@gmail.com>
2021-07-08 10:27:38 +0530

    added io

--- twoPointer.py
+++ twoPointer.py
@@ -41,54 +41,6 @@ def new_algo(data2):
                 else:
                     i += 1
 
-        else:
-            # execute first process
-            cpu = ready[0]
-            ready = ready[1:]
-            time += cpu['bt']
-            print('P' + str(cpu['id']), cpu['bt'])
-
-            cpu['ct'] = time
-            terminated.append(cpu)
-            i = 0
-            while i < len(data2):
-                if data2[i]['at'] <= time:
-                    ready.append(data2[i])
-                    data2.remove(data2[i])
-                else:
-                    i += 1
-            ready = sorted(ready, key=lambda item: item['bt'])
-
-            if ready and ready[-1]['bt'] > threshold:
-                print(ready[-1]['bt'], 'split')
-                new_process = ready[-1].copy()
-
-                new_process['bt'] = ready[-1]['bt'] // 4
-                if ready[-1]['bt'] % 4 != 0:
-                    new_process['bt'] += 1
-
-                ready[-1]['bt'] -= new_process['bt']
-                ready.append(new_process)
-                iteration += 1
-
-            elif ready:
-                # execute last process
-                cpu = ready[-1]
-                ready.remove(cpu)
-                time += cpu['bt']
-                print('P' + str(cpu['id']), cpu['bt'])
-
-                cpu['ct'] = time
-                terminated.append(cpu)
-                i = 0
-                while i < len(data2):
-                    if data2[i]['at'] <= time:
-                        ready.append(data2[i])
-                        data2.remove(data2[i])
-                    else:
-                        i += 1
-                iteration += 1
-
     i = 0
     while i < len(terminated):
         j = 0
